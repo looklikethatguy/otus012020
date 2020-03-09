1c94d540c4bbfe50c14165159578a462cb5ba13d
Ivan Matyukha <krdsev@ya.ru>
2020-03-09 18:59:23 +0300

    Загрузка системы

---
+++ HW6/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+exec 0<>/dev/console 1<>/dev/console 2<>/dev/console
+cat <<'msgend'
+ ___________________
+< I'm dracut module >
+ -------------------
+
+(\__/)
+(='.'=)
+E[:]|||||[:]D
+(")_(")
+
+msgend
+sleep 10
+echo " continuing....
+
